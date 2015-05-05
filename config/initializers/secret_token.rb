8a99798403a1e915e623fdf4aeddb8cbb794e44e
Alexandr <sansmir@mail.ru>
2015-05-05 18:34:30 +0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneProject::Application.config.secret_key_base = '1815ef471717082b079358714227d07b5bb4190236bbacaf6390f461c2e4e9f2fb75791e06f9305c021c01c6e4b0ed2e7bdb922714dddd6a69ab700264360da5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OneProject::Application.config.session_store :cookie_store, key: '_one_project_session'
